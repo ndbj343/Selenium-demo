a759890917e699cea9460e5fab2731626fc9c689
SPQANoviem <noviem.booc@newbrighton.edu.ph>
2025-12-15 12:28:01 +0800

    Some changes

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture(scope="class")
 def setup(request):
     # Set up the MAIN driver
-    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     wait = WebDriverWait(driver, 15)
     driver.implicitly_wait(3)
     driver.maximize_window()
